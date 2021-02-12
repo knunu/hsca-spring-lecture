b8dac86181ca323f70252327d3451e48ea4ac2f4
knunu <burette@hanyang.ac.kr>
2021-02-12 16:18:16 +0900

    Initialize the spring boot project for lecture.

---
+++ src/main/java/hsca/springlecture/HscaSpringLectureApplication.java
@@ -0,0 +1,13 @@
+package hsca.springlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HscaSpringLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HscaSpringLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hsca/springlecture/HscaSpringLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package hsca.springlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HscaSpringLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
